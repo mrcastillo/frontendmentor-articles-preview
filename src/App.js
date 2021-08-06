c6292adc0ec3aa0bf3af04c34173418b76a7c954
Castillo <11235140+mrcastillo@users.noreply.github.com>
2021-08-06 01:29:40 -0400

    Almost Completed Desktop
    Almost completed.

--- src/App.js
+++ src/App.js
@@ -26,43 +26,46 @@ function App() {
           <img src={drawer} />
         </div>
 
-        <div className={"article-card-text"}>
-          <h4>Shift the overall look and feel by adding these wonderful touches to furniture in your home</h4>
-          <p>Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I've got some simple tips to help you make any room feel complete.</p>
+        <div className={"article-card-text-social-container"}>
+          <div className={"article-card-text"}>
+            <h4>Shift the overall look and feel by adding these wonderful touches to furniture in your home</h4>
+            <p>Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I've got some simple tips to help you make any room feel complete.</p>
+          </div>
+
+          <div className={"article-card-social"}>
+            <div className={"article-card-social-img"}>
+              <img src={michelle} />
+            </div>
+            
+            {/* hidden until activated*/}
+            <div className={"article-card-social-active-share"}>
+              <p>SHARE</p>
+            </div>
+
+            <div className={"article-card-social-name-date"}>
+                <p id={"name"}>Michelle Appleton</p>
+                <p id={"date"}>28 Jun 2020</p>
+            </div>
+
+            {/* hidden until activated*/}
+            <div className={"article-card-social-icons"}>
+              <div className={"article-card-social-icon-box"}>
+                <img src={fb} />
+              </div>
+              <div className={"article-card-social-icon-box"}>
+                <img src={tw} />
+              </div>
+              <div className={"article-card-social-icon-box"}>
+                <img src={pi} />
+              </div>
+            </div>
+
+            <div className={"article-card-social-button"} onClick={setActive}>
+                <img src={share} />
+            </div>
+          </div>
         </div>
 
-        <div className={"article-card-social"}>
-          <div className={"article-card-social-img"}>
-            <img src={michelle} />
-          </div>
-          
-          {/* hidden until activated*/}
-          <div className={"article-card-social-active-share"}>
-            <p>SHARE</p>
-          </div>
-
-          <div className={"article-card-social-name-date"}>
-              <p id={"name"}>Michelle Appleton</p>
-              <p id={"date"}>28 Jun 2020</p>
-          </div>
-
-          {/* hidden until activated*/}
-          <div className={"article-card-social-icons"}>
-            <div className={"article-card-social-icon-box"}>
-              <img src={fb} />
-            </div>
-            <div className={"article-card-social-icon-box"}>
-              <img src={tw} />
-            </div>
-            <div className={"article-card-social-icon-box"}>
-              <img src={pi} />
-            </div>
-          </div>
-
-          <div className={"article-card-social-button"} onClick={setActive}>
-              <img src={share} />
-          </div>
-        </div>
       </div>
 
     </div>
